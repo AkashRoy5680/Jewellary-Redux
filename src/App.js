8cc21596c0edf4a515b6012a6a70ece271fc848c
AkashRoy5680 <royakash1272@gmail.com>
2022-08-17 03:17:52 +0200

    list of product shows using redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from './containers/Header';
+import ProductDetail from './containers/ProductDetail';
+import ProductListing from './containers/ProductListing';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+  <div className="App">
+
+  <Header></Header>
+  <Routes>
+    <Route path="/" element={<ProductListing></ProductListing>}></Route>
+    <Route path="/product/:productId" element={<ProductDetail></ProductDetail>}></Route>
+    <Route>404 not Found !!! </Route>
+    </Routes>
+
     </div>
   );
 }
